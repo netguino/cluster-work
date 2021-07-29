327050aee845f15dacc1b23b9c653c2f518e03af
Marc Goujon <netguino@gmail.com>
2021-07-28 19:29:56 -0700

    aaaaaaaaaaaaaaaaargggg

--- setup-metal-lb.sh
+++ setup-metal-lb.sh
@@ -1,9 +1,15 @@
+#Make sure this is enabled
+
+sudo sysctl net.bridge.bridge-nf-call-iptables=1
+
+
+
 # Create NS
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/master/manifests/namespace.yaml$a
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/master/manifests/namespace.yaml
 # create memerlist secrets
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 #apply metalbl manifest
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/master/manifests/metallb.yaml
 
 # apply cm
-kubectl apply -f metal-cm.yml
+kubectl apply -f metallb/metal-cm.yml
